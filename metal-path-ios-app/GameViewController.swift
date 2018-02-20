ff295a7f27219d33300901f9f3d20ee8ac884d96
Astemir Eleev <astemireleev@gmail.com>
2018-02-20 13:26:43 +0300

    Initial Commit

---
+++ metal-path-ios-app/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  metal-path-ios-app
+//
+//  Created by Astemir Eleev on 20/02/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+import MetalKit
+
+// Our iOS specific view controller
+class GameViewController: UIViewController {
+
+    var renderer: Renderer!
+    var mtkView: MTKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let mtkView = view as? MTKView else {
+            print("View of Gameview controller is not an MTKView")
+            return
+        }
+
+        // Select the device to render with.  We choose the default device
+        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+            print("Metal is not supported")
+            return
+        }
+        
+        mtkView.device = defaultDevice
+        mtkView.backgroundColor = UIColor.clear
+
+        guard let newRenderer = Renderer(metalKitView: mtkView) else {
+            print("Renderer cannot be initialized")
+            return
+        }
+
+        renderer = newRenderer
+
+        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+
+        mtkView.delegate = renderer
+    }
+}
