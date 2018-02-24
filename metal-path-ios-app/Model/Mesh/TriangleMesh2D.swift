8906777243c764309396d563f997b5c3e3d5b3cf
Astemir Eleev <astemireleev@gmail.com>
2018-02-24 13:35:55 +0300

    Implemented conformance to refactored MeshProtocol and added documentation

--- metal-path-ios-app/Model/Mesh/TriangleMesh2D.swift
+++ metal-path-ios-app/Model/Mesh/TriangleMesh2D.swift
@@ -15,16 +15,34 @@ struct TriangleMesh2D: MeshProtocol {
 
     // MARK: - Properties
     
+    /// The used order for verticies is counter clockwise:
+    ///             Top Center
+    ///     ------------------------
+    /// Bottom Left ---------- Botton Right
+    /// W component makes the used coordinates homogeneous
     var vertexData: [Float] = [
-        -1.0, -1.0, 0.0, 1.0,
-         1.0, -1.0, 0.0, 1.0,
-         0.0,  1.0, 0.0, 1.0
+        -1.0, -1.0, 0.0, 1.0, // Bottom Left
+         1.0, -1.0, 0.0, 1.0, // Bottom Right
+         0.0,  1.0, 0.0, 1.0  // Top Center
     ]
     
+    /// Computes the size of the vertex array by the following scehem:
+    /// 12 floats (3 vertices, each containing 4 float components) = float.size * number of vertex array components
     var dataSize: Int {
-        get {
-            return vertexData.count * MemoryLayout<Float>.size
+        return vertexData.count * MemoryLayout<Float>.size
+    }
+    
+    var buffer: MTLBuffer!
+    
+    // MARK: - Initializers
+    
+    init?(device: MTLDevice) {
+        
+        guard let buffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: []) else {
+            fatalError(#function + " could not create Metal buffer object")
+            return nil
         }
+        self.buffer = buffer
     }
     
 }
