252bd31e3f6e90fec9b360624a4561f30fecb00e
Astemir Eleev <astemireleev@gmail.com>
2018-02-24 13:35:22 +0300

    Added metal buffer property and declaration for failable initializer

--- metal-path-ios-app/Model/Mesh/MeshProtocol.swift
+++ metal-path-ios-app/Model/Mesh/MeshProtocol.swift
@@ -7,9 +7,18 @@
 //
 
 import Foundation
+import MetalKit
 
 /// The protocol specifies a set of requrements for mesh structures. Typically mesh structures hold a set of verticies, texture coordinates, normals and other mesh related data.
 protocol MeshProtocol {
+    
+    // MARK: - Properties
+    
     var vertexData: [Float] { get set}
     var dataSize: Int { get }
+    var buffer: MTLBuffer! { get }
+    
+    // MARK: - Initializers
+    
+    init?(device: MTLDevice)
 }
