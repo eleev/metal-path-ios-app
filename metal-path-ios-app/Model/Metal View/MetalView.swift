73d56cab6ac439a5e25f59ce8565560140f5e539
Astemir Eleev <astemireleev@gmail.com>
2018-02-24 13:36:46 +0300

    Implemented attachShader(:String, :String) throws -> (:String, :String) method and made a couple of refinements related to documentation and comments

--- metal-path-ios-app/Model/Metal View/MetalView.swift
+++ metal-path-ios-app/Model/Metal View/MetalView.swift
@@ -44,25 +44,58 @@ class MetalView: MTKView {
     ///
     /// - Throws: may throw missing attachments, such as missing drawable target for instance
     private func render() throws {
-        
+        // Create render pass descriptor (RPD)
         let rpd = MTLRenderPassDescriptor()
         let bleenColor = MTLClearColorMake(0, 0.5, 0.5, 1.0)
         rpd.colorAttachments[0].texture = currentDrawable?.texture
         rpd.colorAttachments[0].clearColor = bleenColor
         rpd.colorAttachments[0].loadAction = .clear
         
+        // Create default queues and buffers
         let commandQueue = device?.makeCommandQueue()
         let commandBuffer = commandQueue?.makeCommandBuffer()
         let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: rpd)
         
+        // Make sure that current drawable exsists
         guard let drawable = currentDrawable else {
             debugPrint(#function + " drawable was not specified - the renderer will not be able to commit encodding command buffer and command encoder")
             throw iRenderError.missingDrawable
         }
         
+        // Finish encoding and comming changes
         commandEncoder?.endEncoding()
         commandBuffer?.present(drawable)
         commandBuffer?.commit()
+    }
+    
+    /// Creates metal shader function pair for vertex and fragment shaders from the default Metal library
+    ///
+    /// - Parameters:
+    ///   - vertexShaderName: in-bundle name for vertex metal shader file
+    ///   - fragmentShaderName: in-bundle name for fragment metal shader file
+    /// - Returns: tuple containing initialized instnaces of MTLFunction class for both vertex and fragment shaders
+    /// - Throws: there a couple of possible throws that may occur during the runtime of the function:
+    ///     - couldNotCraeteLibrary
+    ///     - couldNotMakeShaderFunction
+    private func attachShaderPair(vertexShaderName: String, fragmentShaderName: String) throws -> (vertexFunction: MTLFunction, fragmentFunction: MTLFunction){
+        // Make sure that library can be obtaned
+        guard let lib = device?.makeDefaultLibrary() else {
+            debugPrint(#function + " could not make default library object, the method will be thrown")
+            throw iRenderError.couldNotCreateLibrary
+        }
+        // Create vertex shader function
+        guard let vertexFunction = lib.makeFunction(name: vertexShaderName) else {
+            debugPrint(#function + " could not create vertex function")
+            throw iRenderError.couldNotMakeShaderFunction
+        }
+        
+        // Create fragment shader function
+        guard let fragmentFunction = lib.makeFunction(name: fragmentShaderName) else {
+            debugPrint(#function + " could not create fragment function")
+            throw iRenderError.couldNotMakeShaderFunction
+        }
+        
+        return (vertexFunction: vertexFunction, fragmentFunction: fragmentFunction)
     }
     
 }
