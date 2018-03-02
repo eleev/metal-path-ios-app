75d91b97d6858ef579449a8859302af2ff049806
Astemir Eleev <astemireleev@gmail.com>
2018-03-02 19:58:31 +0300

    Created MatrixProtocol file

---
+++ metal-path-ios-app/Model/Math/MatrixProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  MatrixProtocol.swift
+//  metal-path-ios-app
+//
+//  Created by Astemir Eleev on 02/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol MatrixProtocol {
+    
+    // MARK: - Properties
+    
+    var data: [Float] { get }
+    
+    // MARK: - Initializers
+    
+    init()
+}
