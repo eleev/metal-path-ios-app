b6ba262469c1444dbac352d45fa7243074cd6f61
Astemir Eleev <astemireleev@gmail.com>
2018-03-06 18:03:27 +0300

    Created Shader group of structs

---
+++ metal-path-ios-app/Model/Shader/ShaderPair.swift
@@ -0,0 +1,14 @@
+//
+//  ShaderPair.swift
+//  metal-path-ios-app
+//
+//  Created by Astemir Eleev on 06/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+struct ShaderPair {
+    var vertexShader: Shader
+    var fragmentShader: Shader
+}
---
+++ metal-path-ios-app/Model/Shader/ShaderType.swift
@@ -0,0 +1,15 @@
+//
+//  ShaderType.swift
+//  metal-path-ios-app
+//
+//  Created by Astemir Eleev on 06/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+enum ShaderType: String {
+    case vertex
+    case fragment
+    case compute
+}
