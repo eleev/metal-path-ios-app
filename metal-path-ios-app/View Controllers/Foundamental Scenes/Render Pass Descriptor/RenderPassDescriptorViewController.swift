5ef9c7694b6409d251f11975e860ef2635954593
Astemir Eleev <astemireleev@gmail.com>
2018-03-16 21:05:04 +0300

    Changed mesh primitive

--- metal-path-ios-app/View Controllers/Foundamental Scenes/Render Pass Descriptor/RenderPassDescriptorViewController.swift
+++ metal-path-ios-app/View Controllers/Foundamental Scenes/Render Pass Descriptor/RenderPassDescriptorViewController.swift
@@ -29,10 +29,14 @@ class RenderPassDescriptorViewController: UIViewController {
         }
         
         /// Node preperation
-        let triangle = TriangleMesh2D(device: defaultDevice)
-        let node = Node(device: defaultDevice, geometry: triangle!)
+//        let triangle = TriangleMesh2D(device: defaultDevice)
+        let quad = QuadMesh3D(device: defaultDevice)
+        
+        let node = Node(device: defaultDevice, geometry: quad!)
         let defaultMatrix = node.modelMatrix
         node.modelMatrix = Matrix4f.scale(matrix: defaultMatrix, factor: float3(x: 0.5, y: 0.5, z: 0.5))
+//        node.modelMatrix = Matrix4f.rotate(matrix: node.modelMatrix, rotation: float3(x: 1, y: 0, z: 1))
+        
         // TODO: manual buffer memory copy should be optimized
         node.prepareModelBuffer()
         
