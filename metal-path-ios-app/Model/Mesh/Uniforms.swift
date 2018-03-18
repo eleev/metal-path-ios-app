4f1228d2ea3038f784854ef883028253a09d8ee5
Astemir Eleev <astemireleev@gmail.com>
2018-03-18 15:25:45 +0300

    Implemented utility struct called Uniforms

---
+++ metal-path-ios-app/Model/Mesh/Uniforms.swift
@@ -0,0 +1,18 @@
+//
+//  Uniforms.swift
+//  metal-path-ios-app
+//
+//  Created by Astemir Eleev on 18/03/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct Uniforms {
+//    var modelMatrix: matrix_float4x4
+//    var viewMatrix: matrix_float4x4
+//    var projectionMatrix: matrix_float4x4
+    var modelViewProjectionMatrix: matrix_float4x4
+}
+
