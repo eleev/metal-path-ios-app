404ea71a610148902c54c3dfaa63878aad4b2411
Astemir Eleev <astemireleev@gmail.com>
2018-03-18 15:25:21 +0300

    Refactored rotate(:MTLBuffer) function

--- metal-path-ios-app/Model/Animation/Animator.swift
+++ metal-path-ios-app/Model/Animation/Animator.swift
@@ -35,30 +35,23 @@ class Animator: AnimatorProtocol {
     
     // MARK: - Methods
     
-    func rotate(uniformBuffer: MTLBuffer) {
+    func update(uniformBuffer: MTLBuffer) {
         let scaled = Matrix4f.scaling(from: vector3(simd_float2(0.5, 0.5), 0.5))
-        rotation += 1 / speed * Float.pi / 4
+        rotation += 1 / 100 * Float.pi / 4
         
         let rotatedY = Matrix4f.rotation(from: rotation, axis: float3(0, 1, 0))
         let rotatedX = Matrix4f.rotation(from: Float.pi / 4, axis: float3(1, 0, 0))
-        
         let modelMatrix = matrix_multiply(matrix_multiply(rotatedX, rotatedY), scaled)
-        let cameraPosition = vector_float3(0, 0, -3)
+
         
+        let cameraPosition = vector_float3(0, 0, -3)
         let viewMatrix = Matrix4f.translation(from: cameraPosition)
         let projMatrix = Matrix4f.project(near: 0, far: 10, aspect: 1, fovy: 1)
-        
-        let modelViewMatrix = matrix_multiply(modelMatrix, viewMatrix)
-        
-//        let modelViewProjectionMatrix = matrix_multiply(projMatrix, matrix_multiply(viewMatrix, modelMatrix))
+        let modelViewProjectionMatrix = matrix_multiply(projMatrix, matrix_multiply(viewMatrix, modelMatrix))
 
         let bufferPointer = uniformBuffer.contents()
-//        var uniforms = Uniforms(projectionMatrix: modelViewProjectionMatrix)
-        
-//        var uniforms = Uniforms(projectionMatrix: projMatrix, modelViewMatrix: modelViewMatrix)
-        
-        var uniforms = Uniforms(projectionMatrix: projMatrix, modelViewMatrix: modelViewMatrix, modelMatrix: modelMatrix)
-        
+//        var uniforms = Uniforms(modelMatrix: modelMatrix, viewMatrix: viewMatrix, projectionMatrix: projMatrix, modelViewProjectionMatrix: modelViewProjectionMatrix)
+        var uniforms = Uniforms(modelViewProjectionMatrix: modelViewProjectionMatrix)
         memcpy(bufferPointer, &uniforms, MemoryLayout<Uniforms>.size)
     }
     
